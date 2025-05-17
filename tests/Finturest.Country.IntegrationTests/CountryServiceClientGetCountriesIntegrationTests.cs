29677a1879e9a3040852970983e1d113fba0f768
finturest-support <support@finturest.com>
2025-05-18 00:34:58 +0200

    Split tests

---
+++ tests/Finturest.Country.IntegrationTests/CountryServiceClientGetCountriesIntegrationTests.cs
@@ -0,0 +1,71 @@
+﻿using Finturest.Country.Abstractions.Models.Enums;
+
+namespace Finturest.Country.IntegrationTests;
+
+public partial class CountryServiceClientIntegrationTests
+{
+    [Fact]
+    public async Task GetCountriesAsync_RequestIsValid_ReturnCorrectResult()
+    {
+        // Act
+        var result = await _sut.GetCountriesAsync();
+
+        // Assert
+        result.ShouldNotBeEmpty();
+
+        result.Count.ShouldBe(249);
+
+        foreach (var country in result)
+        {
+            country.Name.ShouldNotBeNullOrEmpty();
+            country.LocalName.ShouldNotBeNullOrEmpty();
+            country.Alpha2Code.ShouldNotBeNullOrEmpty();
+            country.Alpha3Code.ShouldNotBeNullOrEmpty();
+            country.NumericCode.ShouldNotBeNullOrEmpty();
+
+            if (country.Region is not null)
+            {
+                country.Region.Code.ShouldNotBeNullOrEmpty();
+                country.Region.Name.ShouldNotBeNullOrEmpty();
+            }
+
+            if (country.Subregion is not null)
+            {
+                country.Subregion.Code.ShouldNotBeNullOrEmpty();
+                country.Subregion.Name.ShouldNotBeNullOrEmpty();
+            }
+
+            if (country.IntermediateRegion is not null)
+            {
+                country.IntermediateRegion.Code.ShouldNotBeNullOrEmpty();
+                country.IntermediateRegion.Name.ShouldNotBeNullOrEmpty();
+            }
+
+            foreach (var currency in country.Currencies)
+            {
+                currency.Code.ShouldNotBeNullOrEmpty();
+                currency.Name.ShouldNotBeNullOrEmpty();
+                currency.NumericCode.ShouldNotBeNullOrEmpty();
+            }
+
+            country.Flag.ShouldNotBeNull();
+
+            country.Flag.Images.ShouldNotBeEmpty();
+            country.Flag.Images.Count.ShouldBe(2);
+
+            country.Flag.Images[0].Format.ShouldBe(FileFormat.SVG);
+            country.Flag.Images[0].Url.ShouldNotBeNullOrEmpty();
+
+            country.Flag.Images[1].Format.ShouldBe(FileFormat.PNG);
+            country.Flag.Images[1].Url.ShouldNotBeNullOrEmpty();
+
+            foreach (var language in country.Languages)
+            {
+                language.Name.ShouldNotBeNullOrEmpty();
+                language.LocalName.ShouldNotBeNullOrEmpty();
+                language.Iso6391Code.ShouldNotBeNullOrEmpty();
+                language.Iso6392Code.ShouldNotBeNullOrEmpty();
+            }
+        }
+    }
+}
--- tests/Finturest.Country.IntegrationTests/CountryServiceClientIntegrationTests.cs
+++ tests/Finturest.Country.IntegrationTests/CountryServiceClientIntegrationTests.cs
@@ -2,7 +2,6 @@
 using System.Net.Http;
 
 using Finturest.Country.Abstractions;
-using Finturest.Country.Abstractions.Models.Enums;
 using Finturest.Country.DependencyInjection;
 
 using Microsoft.Extensions.Configuration;
@@ -13,7 +12,7 @@ namespace Finturest.Country.IntegrationTests;
 /// <summary>
 /// Integration tests for <see cref="ICountryServiceClient"/>
 /// </summary>
-public class CountryServiceClientIntegrationTests
+public partial class CountryServiceClientIntegrationTests
 {
     private readonly ICountryServiceClient _sut;
 
@@ -32,7 +31,7 @@ public class CountryServiceClientIntegrationTests
     }
 
     [Fact]
-    public async Task GetCountriesAsync_ApiKeyIsNotValid_EnsureUnauthorizedStatusCode()
+    public async Task SendRequestAsync_ApiKeyIsNotValid_EnsureUnauthorizedStatusCode()
     {
         // Act
         Func<Task> action = async () => await BuildClient(apiKey: "invalid-api-key").GetCountriesAsync().ConfigureAwait(false);
@@ -45,71 +44,6 @@ public class CountryServiceClientIntegrationTests
 #else
         await action.ShouldThrowAsync<HttpRequestException>();
 #endif
-    }
-
-    [Fact]
-    public async Task GetCountriesAsync_RequestIsValid_ReturnCorrectResult()
-    {
-        // Act
-        var result = await _sut.GetCountriesAsync();
-
-        // Assert
-        result.ShouldNotBeEmpty();
-
-        result.Count.ShouldBe(249);
-
-        foreach (var country in result)
-        {
-            country.Name.ShouldNotBeNullOrEmpty();
-            country.LocalName.ShouldNotBeNullOrEmpty();
-            country.Alpha2Code.ShouldNotBeNullOrEmpty();
-            country.Alpha3Code.ShouldNotBeNullOrEmpty();
-            country.NumericCode.ShouldNotBeNullOrEmpty();
-
-            if (country.Region is not null)
-            {
-                country.Region.Code.ShouldNotBeNullOrEmpty();
-                country.Region.Name.ShouldNotBeNullOrEmpty();
-            }
-
-            if (country.Subregion is not null)
-            {
-                country.Subregion.Code.ShouldNotBeNullOrEmpty();
-                country.Subregion.Name.ShouldNotBeNullOrEmpty();
-            }
-
-            if (country.IntermediateRegion is not null)
-            {
-                country.IntermediateRegion.Code.ShouldNotBeNullOrEmpty();
-                country.IntermediateRegion.Name.ShouldNotBeNullOrEmpty();
-            }
-
-            foreach (var currency in country.Currencies)
-            {
-                currency.Code.ShouldNotBeNullOrEmpty();
-                currency.Name.ShouldNotBeNullOrEmpty();
-                currency.NumericCode.ShouldNotBeNullOrEmpty();
-            }
-
-            country.Flag.ShouldNotBeNull();
-
-            country.Flag.Images.ShouldNotBeEmpty();
-            country.Flag.Images.Count.ShouldBe(2);
-
-            country.Flag.Images[0].Format.ShouldBe(FileFormat.SVG);
-            country.Flag.Images[0].Url.ShouldNotBeNullOrEmpty();
-
-            country.Flag.Images[1].Format.ShouldBe(FileFormat.PNG);
-            country.Flag.Images[1].Url.ShouldNotBeNullOrEmpty();
-
-            foreach (var language in country.Languages)
-            {
-                language.Name.ShouldNotBeNullOrEmpty();
-                language.LocalName.ShouldNotBeNullOrEmpty();
-                language.Iso6391Code.ShouldNotBeNullOrEmpty();
-                language.Iso6392Code.ShouldNotBeNullOrEmpty();
-            }
-        }
     }
 
     private static ICountryServiceClient BuildClient(string apiKey)
