af801b4037cd3b778e98adcf892fb41200187922
finturest-support <support@finturest.com>
2025-05-18 00:08:47 +0200

    Added ICountryServiceClient

---
+++ src/Finturest.Country.Abstractions/ICountryServiceClient.cs
@@ -0,0 +1,17 @@
+﻿using Finturest.Country.Abstractions.Models;
+
+namespace Finturest.Country.Abstractions;
+
+/// <summary>
+/// Provides methods for sending requests to and receiving responses from the Finturest Country API.
+/// </summary>
+public interface ICountryServiceClient
+{
+    /// <summary>
+    /// Get countries
+    /// </summary>
+    /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+    /// <exception cref="ArgumentNullException">The request model was null.</exception>
+    /// <exception cref="HttpRequestException">The request failed due to an underlying issue such as network connectivity, DNS failure, server certificate validation or timeout.</exception>
+    Task<IReadOnlyList<CountryApiModel>> GetCountriesAsync(CancellationToken cancellationToken = default);
+}
