ac0971eb23c7d8cb48904941959fa0a6fa14529b
finturest-support <support@finturest.com>
2025-05-18 00:40:00 +0200

    Added integration tests for get currencies and get languages

---
+++ tests/Finturest.Country.IntegrationTests/CountryServiceClientGetCurrenciesIntegrationTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Finturest.Country.IntegrationTests;
+
+public partial class CountryServiceClientIntegrationTests
+{
+    [Fact]
+    public async Task GetCurrenciesAsync_RequestIsValid_ReturnCorrectResult()
+    {
+        // Act
+        var result = await _sut.GetCurrenciesAsync();
+
+        // Assert
+        result.ShouldNotBeEmpty();
+
+        foreach (var currency in result)
+        {
+            currency.Code.ShouldNotBeNullOrEmpty();
+            currency.Name.ShouldNotBeNullOrEmpty();
+            currency.NumericCode.ShouldNotBeNullOrEmpty();
+
+            foreach (var country in currency.Countries)
+            {
+                country.Name.ShouldNotBeNullOrEmpty();
+                country.Alpha2Code.ShouldNotBeNullOrEmpty();
+                country.Alpha3Code.ShouldNotBeNullOrEmpty();
+                country.NumericCode.ShouldNotBeNullOrEmpty();
+            }
+        }
+    }
+}
---
+++ tests/Finturest.Country.IntegrationTests/CountryServiceClientGetLanguagesIntegrationTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Finturest.Country.IntegrationTests;
+
+public partial class CountryServiceClientIntegrationTests
+{
+    [Fact]
+    public async Task GetLanguagesAsync_RequestIsValid_ReturnCorrectResult()
+    {
+        // Act
+        var result = await _sut.GetLanguagesAsync();
+
+        // Assert
+        result.ShouldNotBeEmpty();
+
+        foreach (var language in result)
+        {
+            language.Name.ShouldNotBeNullOrEmpty();
+            language.LocalName.ShouldNotBeNullOrEmpty();
+            language.Iso6391Code.ShouldNotBeNullOrEmpty();
+            language.Iso6392Code.ShouldNotBeNullOrEmpty();
+
+            foreach (var country in language.Countries)
+            {
+                country.Name.ShouldNotBeNullOrEmpty();
+                country.Alpha2Code.ShouldNotBeNullOrEmpty();
+                country.Alpha3Code.ShouldNotBeNullOrEmpty();
+                country.NumericCode.ShouldNotBeNullOrEmpty();
+            }
+        }
+    }
+}
